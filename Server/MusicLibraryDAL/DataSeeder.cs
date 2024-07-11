77d36f637621dba907d19622c15aae84fce9bb71
sergiu <sergiu.szoke@gmail.com>
2024-07-11 23:14:36 +0300

    Added database seeding

---
+++ Server/MusicLibraryDAL/DataSeeder.cs
@@ -0,0 +1,28 @@
+﻿using MusicLibraryDAL.Models;
+using Newtonsoft.Json;
+
+namespace MusicLibraryDAL
+{
+    public class DataSeeder
+    {
+        private readonly string _jsonFilePath = "../MusicLibraryDAL/data.json";
+        private readonly DataContext _dataContext;
+
+        public DataSeeder(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public void SeedArtist()
+        {
+            if (File.Exists(_jsonFilePath) && !_dataContext.Artists.Any())
+            {
+                var jsonContent = File.ReadAllText(_jsonFilePath);
+                var artists = JsonConvert.DeserializeObject<List<Artist>>(jsonContent);
+
+                _dataContext.AddRange(artists);
+                _dataContext.SaveChanges();
+            }
+        }
+    }
+}
